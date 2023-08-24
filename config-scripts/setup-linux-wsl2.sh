46877d2a14dc75b11e0cff80964f0350c16e2aa7
Gustavo Oliveira <g.alvs.oliveira@gmail.com>
2023-08-23 22:20:18 -0300

    feat: add config scripts and poetry

---
+++ config-scripts/setup-linux-wsl2.sh
@@ -0,0 +1,38 @@
+echo 'Upgrading dependencies ...'
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean
+
+sudo apt install sqlite3
+    
+echo 'Installing asdf ...'
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+echo '. $HOME/.asdf/asdf.sh' >> ~/.bashrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.bashrc
+source ~/.bashrc
+sleep 5
+source ~/.bashrc
+
+echo 'Installing Python ...'
+asdf plugin-add python
+asdf install python 3.11.4
+asdf global python 3.11.4
+
+echo 'Installing Poetry ...'
+asdf plugin-add poetry https://github.com/asdf-community/asdf-poetry.git
+asdf install poetry 1.6.1
+echo 'export PATH="$HOME/.poetry/bin:$PATH"' >> ~/.bashrc
+
+echo 'Updating the project ...'
+git pull
+cd ..
+
+if [ ! -f pyproject.toml ]; then
+    echo 'Creating pyproject.toml ...'
+    poetry init
+fi
+
+echo 'Installing dependencies and pre-commit ...'
+poetry install
+poetry run pre-commit install
