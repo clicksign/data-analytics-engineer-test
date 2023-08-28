bb6d870701e6e8ce19a82bbdea0b57772b311d31
Gustavo Oliveira <g.alvs.oliveira@gmail.com>
2023-08-27 21:55:00 -0300

    fix: add new libs to script

--- config-scripts/setup-linux-wsl2.sh
+++ config-scripts/setup-linux-wsl2.sh
@@ -4,6 +4,9 @@ sudo apt upgrade -y
 sudo apt autoremove -y
 sudo apt autoclean
 
+sudo apt install curl wget openssh-server openssh-client git tar silversearcher-ag unzip -y
+sudo apt-get update; sudo apt-get install --no-install-recommends -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt-get install -y gcc make zlib1g-dev libreadline-dev libreadline8 sqlite3 libsqlite3-dev libbz2-dev python-tk python3-tk tk-dev
 sudo apt install sqlite3
 sudo apt install unrar
     
